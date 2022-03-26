1c75d9e0617c4b7ab9f1327de856641315a66e0e
Abi Johnson <99146064+AbiJays@users.noreply.github.com>
2022-03-26 12:51:07 +0000

    so many notes

--- start_point/tests/profile_test.py
+++ start_point/tests/profile_test.py
@@ -15,9 +15,26 @@ class TestProfile(unittest.TestCase):
         
 
     # Test a Profile can add a favourite Movie
+    def test_add_movie_to_favourites(self): # always only reference self for tests 
+        profile.add_movie(self, new_fave)
+        self.assertEqual(3, len(self.profile.favourites))
+
 
     # Test a Profile can remove a given Movie from favourites
+    @unittest.skip("delete this line to run the test")
+    def test_remove_movie_from_favourites(self):
+        profile.remove_movie(self, old_fave
+        self.assertEqual(2, len(self.profile.favourites))
+
 
     # Test a Profile can return a list of Favourites
+    @unittest.skip("delete this line to run the test")
+    def test_return_favourites(self):
+        profile.return_favourites(self)
+        self.assertEqual(self.profile_1.favourites)
 
    
+# TO DO
+# need to change profile to reference profile_1 object in test
+# make list of self.profile_1's favourites›
+# make new_fave movie object to add.
